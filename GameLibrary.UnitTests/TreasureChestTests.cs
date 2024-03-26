32492e90d8b3923f9f35652bce881e78847b216f
Iain Stewart <iainstewart08@gmail.com>
2024-03-26 15:49:31 +0000

    Add treasure chest tests

--- GameLibrary.UnitTests/TreasureChestTests.cs
+++ GameLibrary.UnitTests/TreasureChestTests.cs
@@ -1,18 +1,54 @@
+using Xunit.Abstractions;
+
 namespace GameLibrary.UnitTests;
 
-public class TreasureChestTests
+public class TreasureChestTests : IDisposable
 {
-    [Fact]
+    private readonly Stack<TreasureChest> chests;
+    private readonly ITestOutputHelper output;
+
+    public TreasureChestTests(ITestOutputHelper output)
+    {
+        // verify that constructor is called before every test
+        chests = new();
+        this.output = output;
+        output.WriteLine($"Initial chest count: {chests.Count}");
+    }
+
+    [Theory]
+    [InlineData(true, true, true)]
+    [InlineData(true, false, false)]
+    [InlineData(false, true, true)]
+    [InlineData(false, false, true)]
+    public void CanOpen_WhenCalled_ReturnsExpectedOutput(bool isLocked, bool hasKey, bool expected)
+    {
+        // arrange
+        var sut = new TreasureChest(isLocked);
+        chests.Push(sut);
+        output.WriteLine($"Chest count: {chests.Count}");
+
+        // act
+        var actual = sut.CanOpen(hasKey);
+
+        // assert
+        Assert.Equal(expected, actual);
+        Assert.Single(chests);
+    }
+
+    [Fact(Skip = "Covered by theory test")]
     public void CanOpen_ChestIsLockedAndHasKey_ReturnsTrue()
     {
         // arrange
         var sut = new TreasureChest(true);
+        chests.Push(sut);
+        output.WriteLine($"Chest count: {chests.Count}");
 
         // act
         var result = sut.CanOpen(true);
 
         // assert
         Assert.True(result);
+        Assert.Single(chests);
     }
 
     [Fact]
@@ -20,12 +56,15 @@ public class TreasureChestTests
     {
         // arrange
         var sut = new TreasureChest(true);
+        chests.Push(sut);
+        output.WriteLine($"Chest count: {chests.Count}");
 
         // act
         var result = sut.CanOpen(false);
 
         // assert
         Assert.False(result);
+        Assert.Single(chests);
     }
 
     [Fact]
@@ -33,12 +72,15 @@ public class TreasureChestTests
     {
         // arrange
         var sut = new TreasureChest(false);
+        chests.Push(sut);
+        output.WriteLine($"Chest count: {chests.Count}");
 
         // act
         var result = sut.CanOpen(true);
 
         // assert
         Assert.True(result);
+        Assert.Single(chests);
     }
 
     [Fact]
@@ -46,11 +88,21 @@ public class TreasureChestTests
     {
         // arrange
         var sut = new TreasureChest(false);
+        chests.Push(sut);
+        output.WriteLine($"Chest count: {chests.Count}");
 
         // act
         var result = sut.CanOpen(false);
 
         // assert
         Assert.True(result);
+        Assert.Single(chests);
+    }
+
+    public void Dispose()
+    {
+        chests.Pop();
+        Assert.Empty(chests);
+        output.WriteLine($"Final chest count: {chests.Count}");
     }
 }
